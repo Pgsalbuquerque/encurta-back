a3e8e39278b8e0d6b906fb6a1b60ec65437d28be
Pgsalbuquerque <albuquerquepgs@gmail.com>
2021-10-12 19:55:29 -0300

    initial configuration

---
+++ src/main/java/com/zg/encurtador/EncurtaBackApplication.java
@@ -0,0 +1,13 @@
+package com.zg.encurtador;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EncurtaBackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EncurtaBackApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/zg/encurtador/EncurtaBackApplicationTests.java
@@ -0,0 +1,13 @@
+package com.zg.encurtador;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EncurtaBackApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
